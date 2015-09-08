3d7d702b16eddd9219e0257c87f6b0341a912b59
CytraL <dev@redneboa.es>
2015-09-08 04:01:25 +0200

    refactoring...

--- src/engine/shared/noise.cpp
+++ src/engine/shared/noise.cpp
@@ -1,25 +1,8 @@
 #include <algorithm>
 #include <ctime> // time()
 
+#include <base/math.h>
 #include "noise.h"
-
-double fade(double t)
-{
-    return t * t * t * (t * (t * 6 - 15) + 10);
-}
-
-double lerp(double t, double a, double b)
-{
-    return a + t * (b - a);
-}
-
-double grad(int hash, double x, double y, double z)
-{
-    int h = hash & 15;
-    double u = h < 8 ? x : y;
-    double v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-}
 
 CPerlin::CPerlin(uint32_t seed)
 {
